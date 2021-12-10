85cf224083eecc16c470fb50f29d3926703b56c1
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-11 05:41:29 +0800

    feat: updated BACKEND_URL.

--- src/modules/urls.mjs
+++ src/modules/urls.mjs
@@ -1,6 +1,4 @@
-// const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
-// const REACT_APP_BACKEND_URL = 'https://mighty-scrubland-94412.herokuapp.com';
-const REACT_APP_BACKEND_URL = 'http://localhost:3004';
-// const REACT_APP_BACKEND_URL = 'https://warm-bayou-69781.herokuapp.com';
+// const REACT_APP_BACKEND_URL = 'http://localhost:3004';
+const REACT_APP_BACKEND_URL = 'https://agile-bastion-48189.herokuapp.com';
 
 export default REACT_APP_BACKEND_URL;
