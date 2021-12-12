40a27379bd3101be5f5175ad59c4c72414662852
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-12 20:09:51 +0800

    feat(Wishes): data preview for Justin.

--- src/components/Wishes/WishListingsPage.jsx
+++ src/components/Wishes/WishListingsPage.jsx
@@ -22,6 +22,283 @@ export default function WishListingsPage() {
         </div>
         <hr />
         <div className="col-12 pt-3" />
+        <div className="row w-100 pt-3">
+          {/* Card 1 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=1"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: justinwong98</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 2: Your Card */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=2"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-dark">Wisher: You!</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-dark">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="text-center">
+                  <div className="badge bg-dark text-wrap w-100">
+                    Note to Justin:
+                    Since you are the wisher of this wish,
+                    you should not see a button here.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 3 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=3"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: shingnan</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 4: Your Card */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=4"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-dark">Wisher: You!</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-dark">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="text-center">
+                  <div className="badge bg-dark text-wrap w-100">
+                    Note to Justin:
+                    Since you are the wisher of this wish,
+                    you should not see a button here.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 5 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=5"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: kai</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 6 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=6"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: chuanxin</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 7 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=7"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: awongh</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 8 */}
+          <div className="col-12 col-sm-6 col-md-3 d-flex">
+            <div className="card w-100 mb-3">
+              <img
+                className="card-img-top img-fluid"
+                src="https://picsum.photos/300/150?random=8"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+
+                <div className="mb-3">
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher: mr_t</span>
+                  </div>
+                  <div className="text-center">
+                    <span className="badge badge-pill bg-success">Wisher Address: abcdef</span>
+                  </div>
+                </div>
+
+                <p className="card-text text-center">
+                  Some quick example text to build on the card title,
+                  and make up the bulk of the card&apos;s content.
+                </p>
+
+                <h4 className="text-center">Price: 0.001 ETH</h4>
+
+                <div className="d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary">Fulfill Wish!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
