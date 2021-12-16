9ca4476622d2d4651300b44099ed3d4fcafbbe60
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-16 21:18:19 +0800

    fix: re-initiate front-end url.

--- src/modules/urls.mjs
+++ src/modules/urls.mjs
@@ -1,4 +1,4 @@
-const REACT_APP_BACKEND_URL = 'http://localhost:3004';
-// const REACT_APP_BACKEND_URL = 'https://agile-bastion-48189.herokuapp.com';
+// const REACT_APP_BACKEND_URL = 'http://localhost:3004';
+const REACT_APP_BACKEND_URL = 'https://agile-bastion-48189.herokuapp.com';
 
 export default REACT_APP_BACKEND_URL;
