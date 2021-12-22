a0cd1787da12e1b7ab5f2c36606e5637da529af3
Lee Chuan Xin <chuanxin@gametize.com>
2021-12-22 09:23:12 +0800

    fix(SearchUsers): only start searching with valid input.

--- src/components/Users/SearchUsersPage.jsx
+++ src/components/Users/SearchUsersPage.jsx
@@ -6,33 +6,48 @@ import { Link } from 'react-router-dom';
 import REACT_APP_BACKEND_URL from '../../modules/urls.mjs';
 import getHash from '../../modules/hashing.mjs';
 
-function UsersSection({ users }) {
-  return users.map((user) => (
-    <div className="col-12 col-md-4">
-      <Link to={`/users/${user.id}`} className="card mb-3 d-block">
-        <div className="card-body">
-          <div className="row">
-            <div className="d-flex justify-content-center align-items-center">
-              <div className="col-1 col-md-3 col-lg-2 me-3">
-                <img
-                  className="img-fluid"
-                  src={`https://avatars.dicebear.com/api/adventurer-neutral/${`${user.id}-${getHash((user.id + 23), user.walletAddress)}`}.svg`}
-                  alt="This is you!"
-                />
+function UsersSection({ users, userInput }) {
+  if (userInput.trim() !== '' && users.length > 0) {
+    return users.map((user) => (
+      <div className="col-12 col-md-4">
+        <Link to={`/users/${user.id}`} className="card mb-3 d-block">
+          <div className="card-body">
+            <div className="row">
+              <div className="d-flex justify-content-center align-items-center">
+                <div className="col-1 col-md-3 col-lg-2 me-3">
+                  <img
+                    className="img-fluid"
+                    src={`https://avatars.dicebear.com/api/adventurer-neutral/${`${user.id}-${getHash((user.id + 23), user.walletAddress)}`}.svg`}
+                    alt="This is you!"
+                  />
+                </div>
+                <div>
+                  <p className="mb-0 text-truncated-parent">
+                    {user.displayName}
+                  </p>
+                </div>
               </div>
-              <div>
-                <p className="mb-0 text-truncated-parent">
-                  {user.displayName}
-                </p>
-              </div>
+
             </div>
-
           </div>
-        </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
+    ));
+  }
 
-  ));
+  if (users.length <= 0) {
+    return (
+      <p className="text-center">
+        There are no users found.
+      </p>
+    );
+  }
+
+  return (
+    <p className="text-center">
+      Type in the search field to find a user!
+    </p>
+  );
 }
 
 export default function SearchUsersPage({ user }) {
@@ -112,6 +127,7 @@ export default function SearchUsersPage({ user }) {
             <div className="row">
               <UsersSection
                 users={filteredUsers}
+                userInput={userInput}
               />
             </div>
           </div>
